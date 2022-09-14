1a2d5d180f146ab49cc80bc367d4fed280968c01
Cameron <Alleyria@gmail.com>
2022-09-14 13:44:03 +0200

    commit class work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  #
+  # post 'restaurants',    to: 'restaurants#create'
+  #
+  # patch "restaurants/:id", to: "restaurants#update"
+  #
+  # delete "restaurants/:id", to: "restaurants#destroy"
+  resources :restaurants
 end
